fe73c150ee99cd0e69e13f8e146d2d1dbd5ccbae
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-17 17:30:31 +0100

    Display a random sudoku puzzle

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'bundler/setup'
 require 'sinatra'
+require_relative './lib/makersacademy/sudoku'
+
+def random_sudoku
+    # we're using 9 numbers, 1 to 9, and 72 zeros as an input
+    # it's obvious there may be no clashes as all numbers are unique
+    seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
+    sudoku = Sudoku.new(seed.join)
+    # then we solve this (really hard!) sudoku
+    sudoku.solve!
+    # and give the output to the view as an array of chars
+    sudoku.to_s.chars
+end
 
 get '/' do
+  @current_solution = random_sudoku
   erb :index
 end
