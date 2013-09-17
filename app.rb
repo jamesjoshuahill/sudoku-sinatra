57036f28172c4792de24d87f697716208e92d94d
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-17 17:59:52 +0100

    Create a new puzzle and solution for a new session

--- app.rb
+++ app.rb
@@ -2,18 +2,32 @@ require 'bundler/setup'
 require 'sinatra'
 require_relative './lib/makersacademy/sudoku'
 
+enable :sessions
+
 def random_sudoku
-    # we're using 9 numbers, 1 to 9, and 72 zeros as an input
-    # it's obvious there may be no clashes as all numbers are unique
-    seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
-    sudoku = Sudoku.new(seed.join)
-    # then we solve this (really hard!) sudoku
-    sudoku.solve!
-    # and give the output to the view as an array of chars
-    sudoku.to_s.chars
+  seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
+  sudoku = Sudoku.new(seed.join)
+  sudoku.solve!
+  sudoku.to_s.chars
+end
+
+def puzzle(sudoku)
+  puzzle = sudoku.dup
+  random_indices = (0..81).to_a.sample(40)
+  random_indices.each do |index|
+    puzzle[index] = ""
+  end
+  puzzle
 end
 
 get '/' do
-  @current_solution = random_sudoku
+  session[:solution] = random_sudoku
+  session[:puzzle] = puzzle(session[:solution])
+  @current_solution = session[:puzzle]
+  erb :index
+end
+
+get '/solution' do
+  @current_solution = session[:solution]
   erb :index
 end
